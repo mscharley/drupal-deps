e2e9b845c00d6f2c41abeba1ae6fcb03e6574982
Matthew Scharley <matt.scharley@gmail.com>
2017-03-20 22:37:09 +1100

    scalafmt

--- src/main/scala/DrupalModule.scala
+++ src/main/scala/DrupalModule.scala
@@ -7,9 +7,9 @@ object DrupalModule {
   private val utf8 = Charset forName "UTF-8"
 
   def fromFile(file: Path): DrupalModule[String] = {
-    val lines = Files.readAllLines(file, utf8).asScala
+    val lines    = Files.readAllLines(file, utf8).asScala
     val filename = file.getFileName.toString
-    val slug = filename.substring(0, filename.length - 5)
+    val slug     = filename.substring(0, filename.length - 5)
 
     fromLines(slug, lines)
   }
@@ -19,9 +19,10 @@ object DrupalModule {
 
   def fromLines(slug: String, lines: Seq[String]): DrupalModule[String] = {
     val config = parseConfig(lines)
-    val name = config.getOrElse("name", Vector(slug))(0)
+    val name   = config.getOrElse("name", Vector(slug))(0)
     val project =
-      if (config.keys.map { _ startsWith "regions" }.foldLeft(false) { _ || _ }) "theme"
+      if (config.keys.map { _ startsWith "regions" }.foldLeft(false) { _ || _ })
+        "theme"
       else if (slug.startsWith("e_") || slug.contains("equiem")) "equiem"
       else config.getOrElse("project", Vector(slug))(0)
     val dependencies = config.getOrElse("dependencies[]", Vector()) map { dep =>
@@ -41,16 +42,20 @@ object DrupalModule {
 
   private val parseLines: (Seq[String]) => Map[String, IndexedSeq[String]] =
     _.map { l =>
-      l.split("=", 2).map { x => x.trim() }
+      l.split("=", 2).map { x =>
+        x.trim()
+      }
     }.foldLeft(Map[String, Vector[String]]().withDefault(_ => Vector())) { (m, l) =>
       val v =
-        if (l(1).length > 2 && l(1)(0) == '\"') l(1).substring(1, l(1).length - 1)
+        if (l(1).length > 2 && l(1)(0) == '\"')
+          l(1).substring(1, l(1).length - 1)
         else l(1)
       m + (l(0) -> (m(l(0)) :+ v))
     }
 }
 
 case class DrupalModule[T](slug: String, name: String, project: String, dependencies: IndexedSeq[T]) {
+
   def mapDependencies[U](f: T => U): DrupalModule[U] =
     copy(dependencies = dependencies.map(f))
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,63 +3,79 @@ import java.nio.file._
 import scala.collection.JavaConverters._
 
 object Main {
+
   def main(args: Array[String]): Unit = {
     val start =
       if (args.length > 0) Paths.get(args(0))
       else Paths.get("./")
 
-    val modules = Files.walk(start).iterator().asScala.filter { f =>
-      f.getFileName.toString.endsWith(".info")
-    }.map {
-      DrupalModule.fromFile
-    }.foldLeft(Map[String, DrupalModule[String]]()) { (m, dm) =>
-      m + (dm.slug -> dm)
-    }
-    val uninstalledDependencies = modules.flatMap {
-      _._2.dependencies
-    }.toSet.filter { m =>
-      !modules.isDefinedAt(m)
-    }.foldLeft(Map[String, DrupalModule[String]]()) { (m, dm) =>
-      m + (dm -> DrupalModule(dm, dm, dm, Vector()))
-    }
+    val modules = Files
+      .walk(start)
+      .iterator()
+      .asScala
+      .filter { f =>
+        f.getFileName.toString.endsWith(".info")
+      }
+      .map {
+        DrupalModule.fromFile
+      }
+      .foldLeft(Map[String, DrupalModule[String]]()) { (m, dm) =>
+        m + (dm.slug -> dm)
+      }
+    val uninstalledDependencies = modules
+      .flatMap {
+        _._2.dependencies
+      }
+      .toSet
+      .filter { m =>
+        !modules.isDefinedAt(m)
+      }
+      .foldLeft(Map[String, DrupalModule[String]]()) { (m, dm) =>
+        m + (dm -> DrupalModule(dm, dm, dm, Vector()))
+      }
 
     val graph = modules.map {
       _._2.mapDependencies(dep => (modules orElse uninstalledDependencies)(dep))
     }
 
-    val projects = modules.foldLeft(Map[String, Map[String, DrupalModule[String]]]().withDefault(_ => Map())) {
-      case (m: Map[String, Map[String, DrupalModule[String]]], (name, dm)) =>
-        m + (dm.project -> (m(dm.project) + (name -> dm)))
-    }
+    val projects =
+      modules.foldLeft(Map[String, Map[String, DrupalModule[String]]]().withDefault(_ => Map())) {
+        case (m: Map[String, Map[String, DrupalModule[String]]], (name, dm)) =>
+          m + (dm.project -> (m(dm.project) + (name -> dm)))
+      }
 
     println("digraph {")
-    projects foreach { case ((project, mods)) =>
-      println(s"  subgraph cluster_${project} {")
-      println(s"""    label="${project}"""")
-      println("""    graph [style="solid,filled",fillcolor="#DADADA"]""")
+    projects foreach {
+      case ((project, mods)) =>
+        println(s"  subgraph cluster_${project} {")
+        println(s"""    label="${project}"""")
+        println("""    graph [style="solid,filled",fillcolor="#DADADA"]""")
 
-      val deps = mods flatMap { case ((m, dm)) =>
-        val slug = m.replace("\"", "\\\"")
-        val name = dm.name.replace("\"", "\\\"")
-        val project = dm.project.replace("\"", "\\\"")
-        val deps = dm.dependencies map { _.replace("\"", "\\\"") }
+        val deps = mods flatMap {
+          case ((m, dm)) =>
+            val slug    = m.replace("\"", "\\\"")
+            val name    = dm.name.replace("\"", "\\\"")
+            val project = dm.project.replace("\"", "\\\"")
+            val deps    = dm.dependencies map { _.replace("\"", "\\\"") }
 
-        println(s"""    "${slug}" [label="${name}",URL="https://drupal.org/project/${project}"]""")
-        deps map { d =>
-          val targetProject = modules.get(d).map(_.project).getOrElse("uninstalled")
-          if (project == targetProject) s"""  ${slug} -> "${d}""""
-          else s"""  "cluster_${project}" -> "cluster_${targetProject}""""
+            println(s"""    "${slug}" [label="${name}",URL="https://drupal.org/project/${project}"]""")
+            deps map { d =>
+              val targetProject =
+                modules.get(d).map(_.project).getOrElse("uninstalled")
+              if (project == targetProject) s"""  ${slug} -> "${d}""""
+              else s"""  "cluster_${project}" -> "cluster_${targetProject}""""
+            }
         }
-      }
-      println("  }")
-      deps.toSet foreach { println }
+        println("  }")
+        deps.toSet foreach { println }
     }
     println("  subgraph cluster_uninstalled {")
     println("""    label="Uninstalled"""")
     println("""    graph [style="solid,filled",fillcolor="#DADADA"]""")
-    uninstalledDependencies foreach { case ((u, dm)) =>
-      val slug = u.replace("\"", "\\\"")
-      println(s"""    "${slug}" [URL="https://drupal.org/project/${slug}"]""")
+    uninstalledDependencies foreach {
+      case ((u, dm)) =>
+        val slug = u.replace("\"", "\\\"")
+        println(s"""    "${slug}" [URL="https://drupal.org/project/${slug}"]""")
     }
     println("  }")
     println("}")
